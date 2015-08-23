4ae098f4eee8b61235338caf3099b7dee77e400d
Zach Rickert <zachrickert@gmail.com>
2015-08-22 20:29:00 -0700

    asks user for the number of iterations

--- chaos.py
+++ chaos.py
@@ -1,10 +1,12 @@
 # A simple program to show chaotic behavior
 
 def main():
-  print("This program illustrates a chaotic function")
-  x = input("Enter a number between 0 and 1: ")
-  for i in range(10):
-    x = 3.9 * x * (1-x)
-    print (x)
+    print("This program illustrates a chaotic function")
+    x = input("Enter a number between 0 and 1: ")
+    number_of_iterations = input("How many numbers shoud I print? ")
+
+    for i in range(number_of_iterations):
+        x = 3.9 * x * (1-x)
+        print (x)
 
 main()
