6ea0bac45f843f6ed7e9f3324d25e5ab48fede35
Zach Rickert <zachrickert@gmail.com>
2015-08-22 21:59:06 -0700

    asks for two inputs and displays the output together

--- chaos.py
+++ chaos.py
@@ -3,10 +3,15 @@
 def main():
     print("This program illustrates a chaotic function")
     x = input("Enter a number between 0 and 1: ")
+    y = input("Enter a second number between 0 and 1: ")
     number_of_iterations = input("How many numbers shoud I print? ")
 
+    print
+    print str("{:0.7f}".format(x)) + " | " + str("{:0.7f}".format(y))
+    print "----------|----------"
     for i in range(number_of_iterations):
         x = 3.9 * x * (1-x)
-        print (x)
+        y = 3.9 * y * (1-y)
+        print str("{:0.7f}".format(x)) + " | " + str("{:0.7f}".format(y))
 
 main()
